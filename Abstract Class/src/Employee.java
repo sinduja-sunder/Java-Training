3a6be3c23910e0ddac2d56ae138db3639adac202
sinduja-sunder <49675899+sinduja-sunder@users.noreply.github.com>
2019-04-16 18:31:49 +0530

    Add files via upload

---
+++ Abstract Class/src/Employee.java
@@ -0,0 +1,24 @@
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+public class Employee extends Trial {
+	
+	void Emp() {
+	 try
+	 {
+		
+		 ResultSet rs1 = st.executeQuery("USE EmployeeManagement; SELECT * FROM EMPLOYEE;");
+		 ResultSetMetaData rsmd1 = rs1.getMetaData();
+		 System.out.println(rsmd1.getColumnName(1)+" "+rsmd1.getColumnName(2)+" "+rsmd1.getColumnName(3)+" "+rsmd1.getColumnName(4)+" "+rsmd1.getColumnName(5)+" "+rsmd1.getColumnName(6)+" "+rsmd1.getColumnName(7));
+		 while(rs1.next()){
+			 
+			 System.out.println(rs1.getInt(1)+ " "+rs1.getString(2)+" "+rs1.getString(3)+" "+rs1.getInt(4)+" "+rs1.getFloat(5)+" "+rs1.getInt(6)+" "+rs1.getInt(7));
+			 System.out.println();
+		}
+	 }
+	 catch(Exception e){
+		 System.out.println(e);
+	     }
+	 }
+}
---
+++ Abstract Class/src/MysqlCon.java
@@ -0,0 +1,16 @@
+
+public class MysqlCon {
+	 public static void main(String[] args) {
+	 
+		 Department D=new Department();
+		 Employee E=new Employee();
+		 skills S=new skills();		 
+		 D.trial();
+		 D.Dep();
+		 E.trial();
+		 E.Emp();
+		 S.trial();
+		 S.ski();
+		 		 
+		 }
+	 }
